00a420b4b4bbae02e7139e23bc2099985d3fc3a1
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2018-06-17 11:21:01 +0200

    balanced fewer_than_half as well

--- sonia/exp1/web/js/experiment.js
+++ sonia/exp1/web/js/experiment.js
@@ -68,7 +68,8 @@ function get_balanced_percents(N) {
 // the below depends on 'Most' and 'More than half' being the first two quantifiers in quants
 var most_percents = get_balanced_percents(AB_pairs.length);
 var mthalf_percents = get_balanced_percents(AB_pairs.length);
-var other_percents = _.map(_.range((quants.length-2)*AB_pairs.length), function() {
+var fthalf_percents = get_balanced_percents(AB_pairs.length);
+var other_percents = _.map(_.range((quants.length-3)*AB_pairs.length), function() {
 	return _.sample(_.without(_.range(1, 100), 50)) });
 var percents = _.flatten([most_percents, mthalf_percents, other_percents])
 var pairs = cartesianProduct(quants, AB_pairs);
