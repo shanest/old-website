6bea5c1cbcdd40f0a9d422410250cee51c17acd8
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2024-03-18 16:23:15 -0700

    initial bib for 574, needs cleaning

---
+++ quantifier-processing/html/js/local_utils.js
@@ -0,0 +1,20 @@
+function cartesianProduct() {
+    return _.reduce(arguments, function(a, b) {
+        return _.flatten(_.map(a, function(x) {
+            return _.map(b, function(y) {
+                return x.concat([y]);
+            });
+        }), true);
+    }, [ [] ]);
+};
+
+function random_choice(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+};
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
