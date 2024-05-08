caa2f503d0c8c7f65c3520d4cf830a55432969e1
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2024-05-08 11:56:02 -0700

    update slides 13

--- sonia/Stability experiment/web/save_data.php
+++
@@ -1,6 +0,0 @@
-<?php
-$txt = $_POST["json"];
-$filename="/var/www/illc/research/cosaqexperiments/data/exp1/".uniqid("fanjingshan_session1_").".json";
-file_put_contents($filename, $txt);
-chmod($filename, 0666)
-?>
